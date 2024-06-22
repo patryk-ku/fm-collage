ff513080003ab80d110252bb3420065d692d9e07
Patryk Kurdziel <patryk.kurdziel@protonmail.com>
2024-06-22 17:51:44 +0200

    fix: scroll resize behavior on mobile

--- app/page.jsx
+++ app/page.jsx
@@ -3,7 +3,7 @@ import Footer from '@/app/ui/Footer';
 
 export default function Home() {
 	return (
-		<div className='grid h-dvh w-screen grid-rows-[1fr] md:grid-rows-[1fr_auto]'>
+		<div className='grid h-screen w-screen grid-rows-[1fr] md:grid-rows-[1fr_auto]'>
 			<div className='grid justify-center p-1 md:content-center md:p-2'>
 				<CollageCreator />
 			</div>
